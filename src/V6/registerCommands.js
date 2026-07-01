7816da801652540593effa9f081dd9232174469b
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 15:17:06 +0530

    to test

--- src/V6/registerCommands.js
+++ src/V6/registerCommands.js
@@ -1,5 +1,13 @@
+import * as vscode from 'vscode';
 import { registerAllCommands as ForEditorTitle } from './ForEditorTitle/registerCommands.js';
+import readHtml from './backend/readHtml.js';
 
 export function registerAllCommands(context) {
+    // 1. Register old InputBox command (AddTableName)
     ForEditorTitle(context);
+
+    // 2. Register new Webview command (AddTableNameHtml)
+    const commandHtml = 'extension.editor.title.routesjs.addTableNameHtml';
+    const showHtml = vscode.commands.registerCommand(commandHtml, (uri) => readHtml(context, uri));
+    context.subscriptions.push(showHtml);
 };
